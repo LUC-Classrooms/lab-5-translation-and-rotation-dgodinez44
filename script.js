32f8813f919bd143706805949d2ed2d18029e3aa
Denise Godinez <119900012+dgodinez44@users.noreply.github.com>
2024-02-08 21:29:39 +0000

    moved image to center and made it rotate

--- script.js
+++ script.js
@@ -14,9 +14,12 @@ function draw() {
   background(200);
   
   push(); // create a new temporary drawing layer
-  translate(0, 0); // change these arguments
+  translate(width/2, height/2); // change these arguments
   
   //add an if() statement here
+  if(mouseIsPressed){
+    rotate(PI/2); // rotate image by a quarter 
+  }
   
   line(0, -50, 40, -20);
   line(0, -50, -40, -20);
